9693dbba321de2273e25d6a6f7227095419d13c4
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2025-12-29 19:49:02 +0530

    Update print statement in Sample.java

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample{
 
 public static void main(String[] args)
   { 
-    System.out.println("Job getting trigger from jenkins DONE!! DONE!!!!!");
+    System.out.println("Job getting trigger from jenkins");
     
   }
 
