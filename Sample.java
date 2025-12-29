d537da9e8f053633e809a72df6b3f99fc8d58c9f
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2025-12-29 20:17:59 +0530

    Update log message for Jenkins job trigger

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample{
 
 public static void main(String[] args)
   { 
-    System.out.println("Job getting trigger from jenkins checking polling from SCM second run");
+    System.out.println("Job getting trigger from jenkins checking polling from SCM second run running using webhook!!");
     
   }
 
