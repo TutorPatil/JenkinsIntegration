35423d3410e00450279d0ff49e9d7564fe5a4143
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2025-12-29 20:02:45 +0530

    Update log message for Jenkins job trigger

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample{
 
 public static void main(String[] args)
   { 
-    System.out.println("Job getting trigger from jenkins");
+    System.out.println("Job getting trigger from jenkins checking polling from SCM");
     
   }
 
